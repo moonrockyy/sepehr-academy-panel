0e96b555c4f116da2c00aa860f5344eeeb17f9ad
Mahan Jafari <mahanjafari14@gmail.com>
2021-11-23 20:58:24 +0330

    Colors

--- src/components/Login/BtnContainer/BtnContainer.jsx
+++ src/components/Login/BtnContainer/BtnContainer.jsx
@@ -5,10 +5,10 @@ const BtnContainer = () => {
   return (
     <div className="btn-container mb-1 d-inline-flex flex-wrap justify-content-center rounded-pill">
       <Link to="/register">
-        <button className={`btn m-1 rounded-pill text-color`}>ثبت نام</button>
+        <button className={`btn m-1 rounded-pill text-white`}>ثبت نام</button>
       </Link>
 
-      <button className={`btn m-1 rounded-pill text-color bg-light`}>
+      <button className={`btn m-1 rounded-pill text-dark bg-white`}>
         ورود ادمین
       </button>
     </div>

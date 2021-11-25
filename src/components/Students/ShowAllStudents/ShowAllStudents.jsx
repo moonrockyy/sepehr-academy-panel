303cbb65a1c3404e79dfb34e4780b88fe3708822
Mahan Jafari <mahanjafari14@gmail.com>
2021-11-25 21:43:40 +0330

    Bugs

--- src/components/Students/ShowAllStudents/ShowAllStudents.jsx
+++ src/components/Students/ShowAllStudents/ShowAllStudents.jsx
@@ -20,47 +20,45 @@ const ShowAllStudents = () => {
   }
   return (
     <>
-      <>
-        <div className="course-body mt-4">
-          <div className={'mainDiv container mt-4 mb-5'}>
-            <h4>{'همه دانش آموزان'} </h4>
-          </div>
-          <table className="table panel-table table-borderless ">
-            <thead className={'thead-color'}>
-              <tr className={'tr-color'}>
-                <th scope="col">نام دانش آموز</th>
-                <th scope="col">ایمیل</th>
-                <th scope="col">شماره موبایل</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {allStudentsData.map((student) => (
-                <tr key={student._id} className={'green-hover'}>
-                  <td scope="row" className={'course-td-items-topics'}>
-                    {student.fullName}
-                  </td>
-                  <td scope="row" className={'course-td-items'}>
-                    {student.email}
-                  </td>
-                  <td scope="row" className={'course-th-items'}>
-                    {student.phoneNumber}
-                  </td>
-
-                  <td scope="row" className={'course-th-items'}>
-                    <button
-                      onClick={() => handleDelete(student)}
-                      className="btn btn-outline-danger btn-sm"
-                    >
-                      حذف
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+      <div className="course-body mt-4">
+        <div className='mainDiv container mt-4 mb-5'>
+          <h4>{'همه دانش آموزان'}</h4>
         </div>
-      </>
+        <table className="table panel-table table-borderless ">
+          <thead className={'thead-color'}>
+            <tr className={'tr-color'}>
+              <th scope="col">نام دانش آموز</th>
+              <th scope="col">ایمیل</th>
+              <th scope="col">شماره موبایل</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {allStudentsData.map((student) => (
+              <tr key={student._id} className={'green-hover'}>
+                <td scope="row" className={'course-td-items-topics'}>
+                  {student.fullName}
+                </td>
+                <td scope="row" className={'course-td-items'}>
+                  {student.email}
+                </td>
+                <td scope="row" className={'course-th-items'}>
+                  {student.phoneNumber}
+                </td>
+
+                <td scope="row" className={'course-th-items'}>
+                  <button
+                    onClick={() => handleDelete(student)}
+                    className="btn btn-outline-danger btn-sm"
+                  >
+                    حذف
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   )
 }
